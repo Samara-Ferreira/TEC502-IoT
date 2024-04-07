d3a7371a40b145fe948eb4b2dc73c49011b42a28
Samara <sferreira@ecomp.uefs.br>
2024-04-07 12:00:55 -0300

    :recycle: fix: correções no código do servidor

--- PBL/Server/server.py
+++ PBL/Server/server.py
@@ -1,13 +1,14 @@
 '''
 DESCRIÇÃO 
-- este código é um servidor que fará comunicações com dispositivos
-- o envio de comandos para os dispositivos será via sockets (TCP)
-- o recebimento de informações dos dispositivos para o servidor será via sockets (UDP)
-- o servidor possui um IP e uma porta, sendo o IP = localhost e a porta = 5000
+- servidor que fará comunicação via TCP/UDP com os dispositivos, e comunicação via HTTP com a aplicação 
+com a API RESTful para a aplicação
+- o recebimento de informação do dispositivo será via sockets (UDP)
+- o envio de comandos para o dispositivo será via sockets (TCP)
+- a comunicação com a aplicação será via HTTP
 '''
-
 import threading
 import socket
+import requests # Importar a biblioteca requests para comunicação HTTP
 
 # Lista de dispositivos conectados
 devicesConnected = []
@@ -16,68 +17,53 @@ HOST = "localhost"
 PORT = 5000
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # sockets para comunicação TCP e UDP
+    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+    udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # UDP
 
-    try:
-        # Vinculando os sockets à mesma porta
-        server.bind((HOST, PORT))
-        server.listen(5)
-        udp_server.bind((HOST, PORT))
+    try: 
+        # conexão dos sockets à mesma porta
+        tcp_server.bind((HOST, PORT))
+        tcp_server.listen(5)    # máximo de 5 conexões
+        udp_server.bind((HOST, PORT))   
         print("Servidor iniciado!\n")
     except:
-        print(f"Erro ao conectar ao servidor.\n")
-        return  # Sair da função se não for possível conectar ao servidor
+        return print(f"Erro ao conectar ao servidor.\n")
+        
+    # threads para lidar com as conexões TCP e UDP 
+    threading.Thread(target=deviceConnection, args=[tcp_server]).start()
+    # criando uma thread para receber os dados do dispositivo via UDP
+    threading.Thread(target=udpDataDevice, args=[udp_server]).start()
 
-    # Iniciar threads para lidar com as conexões TCP e UDP
-    threading.Thread(target=tcpConnections, args=[server]).start()
-    threading.Thread(target=udpData, args=[udp_server]).start()
-
-def tcpConnections(server):
+def deviceConnection(tcp_server):
     while True:
         try:
-            print("Aguardando conexões TCP...\n")
-            client, address = server.accept()  # Aceitar conexão do cliente
-            devicesConnected.append(client)
+            print("Aguardando conexões...\n")
+            device, address = tcp_server.accept()  # aceitar conexão do dispositivo
+            devicesConnected.append(device)
             print(f"Conexão estabelecida com {address}\n")
             print(f"Dispositivos conectados: {devicesConnected}\n")
-
-            # Iniciar uma nova thread para lidar com o cliente
-            threading.Thread(target=tcpClient, args=[client]).start()
 
         except:
             print(f"Erro ao aceitar conexão.\n")
 
-def tcpClient(client):
+# faça para receber os dados do dispositivo via UDP
+def udpDataDevice(udp_server):
     while True:
         try:
-            data_message = client.recv(2048).decode("utf-8")  # Receber dados do cliente
-
-            print(f"Mensagem recebida via TCP de {client}: {data_message}\n")
-
-            for client in devicesConnected:
-                if client != client:
-                    try:
-                        client.send(data_message.encode("utf-8"))  # Enviar mensagem para o cliente
-                    
-                    except:
-                        print(f"Erro ao enviar mensagem para o cliente.\n")
-                        devicesConnected.remove(client)
-
-            # Adicionar lógica para responder ao cliente, se necessário
-
+            data_device, address = udp_server.recvfrom(2048)  # receber dados do dispositivo
+            print(f"Dados do dispositivo {address}: {data_device.decode('utf-8')}\n")
         except:
-            print(f"Erro ao receber mensagem do cliente.\n")
-            client.close()
-            devicesConnected.remove(client)
-            break
+            print(f"Erro ao receber dados do dispositivo.\n")
 
-def udpData(udp_server):
-    while True:
-        try:
-            data, address = udp_server.recvfrom(2048)  # Receber dados via UDP
-            print(f"Mensagem recebida de {address}: {data.decode('utf-8')}\n")
-        except:
-            print(f"Erro ao receber mensagem via UDP.\n")
+
+# receber os comandos da aplicação via HTTP
+def receiveDataApplication():
+    pass
+
+# enviar os dados dos dispositivos para a aplicação via HTTP
+def sendDataApplication():
+    pass
+
 
 main()
