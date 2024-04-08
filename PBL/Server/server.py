12c0045fcc45b4bcb1edbdbbd2a16a576332af0a
Samara <sferreira@ecomp.uefs.br>
2024-04-08 14:55:42 -0300

    :recycle: fix: correções no código do servidor

--- PBL/Server/server.py
+++ PBL/Server/server.py
@@ -12,20 +12,22 @@ import requests # Importar a biblioteca requests para comunicação HTTP
 
 # Lista de dispositivos conectados
 devicesConnected = []
+socketsList = []
 
 HOST = "localhost"
-PORT = 5000
+PORT_TCP = 5000
+PORT_UDP = 5000
+udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # UDP
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
+
 
 def main():
     # sockets para comunicação TCP e UDP
-    tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
-    udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)   # UDP
-
     try: 
-        # conexão dos sockets à mesma porta
-        tcp_server.bind((HOST, PORT))
+        # conexão dos sockets à mesma PORT_TCPa
+        tcp_server.bind((HOST, PORT_TCP))
         tcp_server.listen(5)    # máximo de 5 conexões
-        udp_server.bind((HOST, PORT))   
+        udp_server.bind((HOST, PORT_UDP))   
         print("Servidor iniciado!\n")
     except:
         return print(f"Erro ao conectar ao servidor.\n")
@@ -33,19 +35,27 @@ def main():
     # threads para lidar com as conexões TCP e UDP 
     threading.Thread(target=deviceConnection, args=[tcp_server]).start()
     # criando uma thread para receber os dados do dispositivo via UDP
-    threading.Thread(target=udpDataDevice, args=[udp_server]).start()
+    #threading.Thread(target=udpDataDevice, args=[udp_server]).start()
 
 def deviceConnection(tcp_server):
     while True:
         try:
             print("Aguardando conexões...\n")
             device, address = tcp_server.accept()  # aceitar conexão do dispositivo
-            devicesConnected.append(device)
+            devicesConnected.append({"ip": address[0]})
+            socketsList.append(device)
             print(f"Conexão estabelecida com {address}\n")
             print(f"Dispositivos conectados: {devicesConnected}\n")
 
         except:
             print(f"Erro ao aceitar conexão.\n")
+
+
+def sendCommandDevice():
+    socketsList[0].send("Iniciar".encode("utf-8"))
+    print("Server: Comando enviado para o dispositivo.\n")
+    data_device, address = udp_server.recvfrom(2048)
+    return {"hgg":data_device.decode("utf-8")}
 
 # faça para receber os dados do dispositivo via UDP
 def udpDataDevice(udp_server):
@@ -53,6 +63,7 @@ def udpDataDevice(udp_server):
         try:
             data_device, address = udp_server.recvfrom(2048)  # receber dados do dispositivo
             print(f"Dados do dispositivo {address}: {data_device.decode('utf-8')}\n")
+            return {"Dados": data_device.decode("utf-8")}
         except:
             print(f"Erro ao receber dados do dispositivo.\n")
 
@@ -66,4 +77,5 @@ def sendDataApplication():
     pass
 
 
-main()
+if __name__ == "__main__":
+    main()
