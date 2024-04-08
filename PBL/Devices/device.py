a6899e7bb285d38f1375ff73905eb808ee8c4873
Samara <sferreira@ecomp.uefs.br>
2024-04-08 14:56:12 -0300

    :recycle: fix: correções no código do dispositivo

--- PBL/Devices/device.py
+++ PBL/Devices/device.py
@@ -7,43 +7,57 @@ DESCRIÇÃO
 import threading
 import socket
 
+PORT_TCP = 5000
+PORT_UDP = 5000
+
 def main():
-    udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    udp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     try:
-        tcp_client.connect(("localhost", 5000))
+        tcp_client.connect(("localhost", PORT_TCP))
     except:
         return print("\nErro ao conectar ao servidor.\n")
     
     username = input("Digite seu nome de usuário: ")
     print("Usuário conectado ao servidor.\n")
 
-    receive_thread = threading.Thread(target=receiveMessage, args=[tcp_client])
-    send_thread = threading.Thread(target=sendMessage, args=[udp_client, username])
+    receive_thread = threading.Thread(target=receiveMessage, args=[tcp_client, udp_client])
+    
 
     receive_thread.start()
-    send_thread.start()
+    #send_thread.start()
 
     receive_thread.join()
-    send_thread.join()  
+    #send_thread.join()  
 
-    udp_client.close()
     tcp_client.close()
+    udp_client.close()
 
-def receiveMessage(tcp_client):
+
+def receiveMessage(tcp_client, udp_client):
     while True:
         try:
             message = tcp_client.recv(2048).decode("utf-8")
-            print(f"Mensagem recebida do servidor: {message}\n")
+            print(f"Device: Mensagem recebida do servidor {message}.\n")
+            commandsMessage(udp_client, message)
         except:
             break
 
-def sendMessage(udp_client, username):
-    while True:
-        message = input("Digite a mensagem: ")
-        #udp_client.sendto(message.encode("utf-8"), ("localhost", 5000))
-        udp_client.sendto(f"<{username}> {message}\n".encode("utf-8"), ("localhost", 5000))
+def sendMessage(udp_client, message):      
+    #udp_client.sendto(f"<{username}> {message}\n".encode("utf-8"), ("localhost", PORT_TCP))
+    udp_client.sendto(message.encode("utf-8"), ("localhost", PORT_UDP))
 
-main()
+
+def commandsMessage(udp_client, message):
+    # pensando na ideia de ter uma lista de comandos
+    print("Device: Comando de iniciar do dispositivo recebido!\n")
+
+    threading.Thread(target=sendMessage, args=[udp_client, message]).start()
+
+    #sendMessage(udp_client, message)
+
+
+if __name__ == "__main__":
+    main()
 
