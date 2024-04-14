c79518465142ce43f26cdd829a24ec94d9275fc2
Samara <sferreira@ecomp.uefs.br>
2024-04-14 11:21:17 -0300

    :recycle: fix: modificações no código do sensor

--- Mod/sensor.py
+++ Mod/sensor.py
@@ -15,10 +15,12 @@ HOST = "localhost"
 TCP_PORT = 5001
 UDP_PORT = 5002
 
-tcp_device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-udp_device = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcp_sensor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+udp_sensor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-device = {"id": "SET01", 
+sensor = {"address": 0, 
+        "ip": 0,
+        "id": "SET01",
         "data": 0,
         "categoria": "sensor",
         "status": False}
@@ -26,65 +28,68 @@ device = {"id": "SET01",
 def main():
     # conexão com o servidor
     try:
-        tcp_device.connect((HOST, TCP_PORT))
-        print("Dispositivo conectado ao servidor.\n")
-    except:
-        return print("Erro ao conectar ao servidor.\n")
+        tcp_sensor.connect((HOST, TCP_PORT))
+        setAddress(tcp_sensor.getsockname()[1])
+        print(f"Conexão com o servidor estabelecida com o sensor!\n")
+    except Exception as e:
+        return print(f"Erro ao conectar o sensor ao servidor: {e}\n")
     
     threading.Thread(target=receiveTCP, args=[]).start()
+    threading.Thread(target=menuSensor, args=[]).start()
 
-    threading.Thread(target=menuDevice, args=[]).start()
+def setAddress(address):
+    sensor["address"] = address
 
-def visualizarDados():
-    print("\nDADOS DO DISPOSITIVO:\n")
-    print(f"ID: {device['id']}")
-    print(f"Temperatura: {device['data']}")
-    print(f"Categoria: {device['categoria']}")
-    print(f"Status: {device['status']}")
-    print("\n")
+def turnOnSensor():
+    sensor["status"] = True
+    sendTCP("CONFIRMAÇÃO: SENSOR LIGADO\n")
 
-def getId():
-    return device["id"]
+def turnOffSensor():
+    sensor["status"] = False
+    sendTCP("CONFIRMAÇÃO: SENSOR DESLIGADO\n")
 
-def ligarSensor():
-    device["status"] = True
-    sendTCP("CONFIRMAÇÃO: SENSOR LIGADO")
-
-def desligarSensor():
-    device["status"] = False
-    sendTCP("CONFIRMAÇÃO: SENSOR DESLIGADO")
-
-def mudarTemperatura(temperatura):
-    if (device["status"] == False):
+def changeTemperature(temperature):
+    if (sensor["status"] == False):
         print("O sensor está desligado.\n")
     else:
-        device["data"] = temperatura
+        sensor["data"] = temperature
         visualizarDados()
-        sendTCP(f"CONFIRMAÇÃO: TEMPERATURA ALTERADA PARA {temperatura}")
+        sendTCP(f"CONFIRMAÇÃO: TEMPERATURA ALTERADA PARA {temperature}\n")
 
-def menuDevice():
+def viewData():
+    print("\nDADOS DO SENSOR:\n")
+    print(f"Endereço: {sensor['address']}")
+    print(f"IP: {sensor['ip']}")
+    print(f"ID: {sensor['id']}")
+    print(f"Temperatura: {sensor['data']}")
+    print(f"Categoria: {sensor['categoria']}")
+    print(f"Status: {sensor['status']}")
+    print("\n")
+
+def menuSensor():
     while True:
-        print("\nMENU:\n")
-        print("[1] Ligar\n")
-        print("[2] Desligar\n")
-        print("[3] Retornar dados\n")
+        print("\nMENU DO SENSOR\n")
+        print("[1] Ligar o sensor\n")
+        print("[2] Desligar o sensor\n")
+        print("[3] Pedir a temperatura\n")
         print("[4] Visualizar os dados\n")
         print("[5] Encerrar o programa\n")
 
         # obtendo o comando por meio do terminal
-        option = input("Digite a opção desejada: ")
+        option = input("Digite a opção desejada [sensor]: ")
 
         if (option == "1"):
-            ligarSensor()
+            turnOnSensor()
         elif (option == "2"):
-            desligarSensor()
+            turnOffSensor()
         elif (option == "3"):
-            temperatura = input("Digite a temperatura atual: ")
-            mudarTemperatura(temperatura)
+            temp = input("Digite a temperatura atual: ")
+            changeTemperature(temp)
         elif (option == "4"):
-            visualizarDados()
+            viewData()
         elif (option == "5"):
-            print("Encerrando o programa.\n")
+            print("Encerrando o programa...\n")
+            tcp_sensor.close()
             break
         else:
             print("Opção inválida.\n")
@@ -92,45 +97,52 @@ def menuDevice():
 def receiveTCP():
     while True:
         try:
-            message = tcp_device.recv(2048).decode("utf-8")
-            print(f"Mensagem recebida do servidor: {message}.\n")
+            messageReceive = tcp_sensor.recv(2048).decode("utf-8")
+            print(f"Dados recebidos do servidor [sensor]: {messageReceive}\n")
 
-            command, data = message.split(":")
+            command, data = messageReceive.split(":")
+
+            
 
             if (command == "1"):
-                ligarSensor()
+                turnOnSensor()
+                sendTCP("CONFIRMAÇÃO: SENSOR LIGADO\n")
             elif (command == "2"):
-                desligarSensor()
+                turnOffSensor()
+                sendTCP("CONFIRMAÇÃO: SENSOR DESLIGADO\n")
             elif (command == "3"):
-                send_thread = threading.Thread(target=sendUDP, args=[data])
-                send_thread.start()
-                mudarTemperatura(data)
+                changeTemperature(data)
+                threading.Thread(target=sendUDP, args=[data]).start()
             elif (command == "4"):
-                visualizarDados()
+                viewData()
             elif (command == "5"):
                 print("Encerrando o programa.\n")
+                sendTCP("CONFIRMAÇÃO: PROGRAMA ENCERRADO\n")
                 break
             else:
                 print("Comando inválido.\n")
 
         except Exception as e:
-            print(f"Erro ao receber mensagem do servidor: {e}\n")
+            print(f"Erro ao receber mensagem do servidor [sensor]: {e}\n")
+            break
 
+# enviar mensagem para o servidor via TCP
 def sendTCP(message):
     try:
-        tcp_device.send(message.encode("utf-8"))
+        tcp_sensor.send(message.encode("utf-8"))
     except Exception as e:
         print(f"Erro ao enviar mensagem para o servidor via TCP: {e}\n")
 
+# enviar mensagem para o servidor via UDP
 def sendUDP(message):
     while True:
         try: 
-            udp_device.sendto(message.encode("utf-8"), (HOST, UDP_PORT))
+            # message = f"{sensor['id']}:{sensor['data']}"
+            udp_sensor.sendto(message.encode("utf-8"), (HOST, UDP_PORT))
             time.sleep(5)  
         except Exception as e:
             print(f"Erro ao enviar mensagem para o servidor via UDP: {e}\n")  
-
-
+            break
 
 
 if __name__ == "__main__":
