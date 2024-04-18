b26e8d13b52a4e2060639d9853cffe1280128fd1
Samara <sferreira@ecomp.uefs.br>
2024-04-18 13:33:04 -0300

    :recycle: refactor: modificações no código do atuador

--- atuador.py
+++ atuador.py
@@ -1,168 +1,172 @@
-'''
-DESCRIÇÃO: Geladeira 
-- Funcionalidades: ligar, desligar, mudar o valor atual da temperatura,
-retornar a temperatura atual, visualizar os dados,
-'''
-
-# bibliotecas necessárias 
+# importação de bibliotecas necessárias
 import threading
 import socket
 import time
+import os
 
 # variáveis globais
 HOST = "localhost"
 TCP_PORT = 5001
 UDP_PORT = 5002
 
-threads = []
-
 # sockets para comunicação com o servidor
 tcp_frigde = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 udp_frigde = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
 # dicionário para armazenar os dados da geladeira
 frigde = {"ip": 0,
-        "id": "GEL01",
-        "data": 0,
-        "categoria": "atuador",
-        "status": False}
+          "id": "GEL01",
+          "data": 0.0,
+          "category": "atuador",
+          "status": False}
 
-def setIP(ip):
-    frigde["ip"] = ip
 
-def turnOnFrigde():
-    frigde["status"] = True
-    sendTCP("CONFIRMAÇÃO: GELADEIRA LIGADA\n")
+# função principal
+def main():
+    try:
+        tcp_frigde.connect((HOST, TCP_PORT))
+        threading.Thread(target=receive_tcp, args=[]).start()
+        menu_frigde()
+    except Exception as e:
+        print(f"Erro ao conectar com o servidor: {e}\n")
+        close_program()
 
-def turnOffFrigde():
-    frigde["status"] = False
-    sendTCP("CONFIRMAÇÃO: GELADEIRA DESLIGADA\n")
 
-def changeTemperature(temperature):
-    if (frigde["status"] == False):
-        print("A geladeira está desligada, não é possível mudar os dados!\n")
-    else:
-        frigde["data"] = temperature
-        sendTCP(f"CONFIRMAÇÃO: TEMPERATURA ALTERADA PARA {temperature}\n")
-
-def getTemperature():
-    return frigde["data"]
-
-def viewData():
-    print("\n----DADOS DA GELADEIRA----\n")
-    print(f"Endereço: {frigde['address']}")
-    print(f"IP: {frigde['ip']}")
-    print(f"ID: {frigde['id']}")
-    print(f"Temperatura: {frigde['data']}")
-    print(f"Categoria: {frigde['categoria']}")
-    print(f"Status: {frigde['status']}")
-    print("\n")
-
-# menu para a geladeira no terminal do dispositivo 
-def menuFrigde():
+# menu para a geladeira no terminal do dispositivo
+def menu_frigde():
     while True:
-        print("\n--MENU DA GELADEIRA\n")
+        os.system("cls")
+
+        print("\n----MENU DA GELADEIRA----\n")
         print("[1] Ligar geladeira\n")
         print("[2] Desligar geladeira\n")
         print("[3] Mudar temperatura\n")
         print("[4] Retornar a temperatura\n")
-        print("[5] Visualizar os dados\n")
-        print("[6] Encerrar o programa\n")
-        
-        option = input("Digite a opção desejada: ")
-        
-        if (option == "1"):
-            turnOnFrigde()
-        elif (option == "2"):
-            turnOffFrigde()
-        elif (option == "3"):
-            temp = input("Digite a nova temperatura: ")
-            while (not(temp.isdigit)):
-                print("Digite um valor numérico para a temperatura.\n")
-                temp = input("Digite a nova temperatura: ")
-            changeTemperature(temp)
-        elif (option == "4"):
-            print("Temperatura atual: {getTemperature()}\n")
-        elif (option == "5"):
-            viewData()
-        elif (option == "6"):
-            print("Encerrando o programa...\n")
-            closeProgram()
+        print("[5] Visualizar os dados da geladeira\n")
+        print("[0] Encerrar programa\n")
+        option = str(input("Digite a opção desejada: "))
+
+        if option == "1":
+            turn_on_frigde()
+        elif option == "2":
+            turn_off_frigde()
+        elif option == "3":
+            temperature = float(input("Digite a temperatura desejada: "))
+            change_temperature(temperature)
+        elif option == "4":
+            print(f"Temperatura atual: ", round(get_temperature(), 2), "°C\n")
+        elif option == "5":
+            view_data()
+        elif option == "0":
+            close_program()
             break
         else:
-            print("Opção inválida.\n")
+            print("Opção inválida!\n")
+            time.sleep(2)
 
-def receiveTCP():
-    keep_alive = True
-    while (keep_alive):
-        try: 
-            messageReceive = tcp_frigde.recv(2048).decode("utf-8")
-            command, data = messageReceive.split(":")
 
-            if (command == "1"):
-                turnOnFrigde()
-                sendTCP("CONFIRMAÇÃO: GELADEIRA LIGADA\n")
-            elif (command == "2"):
-                turnOffFrigde()
-                sendTCP("CONFIRMAÇÃO: GELADEIRA DESLIGADA\n")
-            elif (command == "3"):
-                changeTemperature(data)
-                send_thread = threading.Thread(target=sendUDP, args=[data]).start()
-                threads.append(send_thread)
-            elif (command == "4"):
-                sendTCP(f"CONFIRMAÇÃO: TEMPERATURA ATUAL: {getTemperature()}\n")
-            elif (command == "5"): 
-                viewData()
-            elif (command == "6"):
-                print("Encerrando o programa...\n")
-                sendTCP("CONFIRMAÇÃO: PROGRAMA ENCERRADO\n")
-                keep_alive = False
-                break
-            else:
-                print("Comando inválido.\n")
+# função para setar o endereço da geladeira
+def set_ip(ip):
+    frigde["ip"] = ip
 
-        except Exception as e:
-            print(f"Erro ao receber dados do servidor via TCP: {e}\n")
-            keep_alive = False
 
-def sendTCP(message):
+# função para ligar a geladeira
+def turn_on_frigde():
+    frigde["status"] = True
+
+
+# função para desligar a geladeira
+def turn_off_frigde():
+    frigde["status"] = False
+
+
+# função para retornar a temperatura atual
+def get_temperature():
+    return frigde["data"]
+
+
+# função para mudar a temperatura da geladeira
+def change_temperature(temperature):
+    if frigde["status"] is False:
+        print("A geladeira está desligada, não é possível mudar os dados!\n")
+    else:
+        frigde["data"] = temperature
+
+
+# função para visualizar os dados da geladeira
+def view_data():
+    print("\n----DADOS DA GELADEIRA----\n")
+    print(f"Endereço: {frigde['ip']}")
+    print(f"ID: {frigde['id']}")
+    print(f"Temperatura: {frigde['data']}°C")
+    print(f"Categoria: {frigde['category']}")
+    print(f"Status: {frigde['status']}")
+    print("\n")
+
+
+# função para encerrar o programa
+def close_program():
+    print("Encerrando o programa...\n")
+    tcp_frigde.close()
+    udp_frigde.close()
+    exit()
+
+
+# função para enviar uma mensagem via TCP
+def send_tcp(message):
     try:
         tcp_frigde.send(message.encode("utf-8"))
     except Exception as e:
-        print(f"Erro ao enviar comando para o servidor via TCP: {e}\n")
+        print(f"Erro ao enviar mensagem via TCP: {e}\n")
 
-def sendUDP(message):
+
+# função para enviar uma mensagem via UDP
+def send_udp(message):
     while True:
         try:
             udp_frigde.sendto(message.encode("utf-8"), (HOST, UDP_PORT))
             time.sleep(5)
         except Exception as e:
-            print(f"Erro ao enviar dados para o servidor via UDP: {e}\n")
+            print(f"Erro ao enviar mensagem via UDP: {e}\n")
             break
 
-def closeProgram():
-    for thread in threads:
-        thread.join()
 
-    tcp_frigde.close()
-    udp_frigde.close()
+# função para receber uma mensagem via TCP
+def receive_tcp():
+    while True:
+        try:
+            data = tcp_frigde.recv(2048).decode("utf-8")
+            command, data = data.split(":")
 
-# função principal
+            if command == "1":
+                turn_on_frigde()
+                send_tcp("CONFIRMAÇÃO: GELADEIRA LIGADA\n")
+            elif command == "2":
+                turn_off_frigde()
+                send_tcp("CONFIRMAÇÃO: GELADEIRA DESLIGADA\n")
+            elif command == "3":
+                if frigde["status"] is False:
+                    send_tcp("A geladeira está desligada, não é possível mudar os dados!\n")
+                else:
+                    change_temperature(float(data))
+                    threading.Thread(target=send_udp, args=[data]).start()
+                    send_tcp("CONFIRMAÇÃO: TEMPERATURA ALTERADA\n")
+            elif command == "4":
+                send_tcp(f"CONFIRMAÇÃO: TEMPERATURA ATUAL: {get_temperature()}\n")
+            elif command == "5":
+                send_tcp(str(frigde))
+            elif command == "0":
+                send_tcp("CONFIRMAÇÃO: PROGRAMA ENCERRADO\n")
+                close_program()
+                break
 
-try:
-    tcp_frigde.connect((HOST, TCP_PORT))
-    setIP(tcp_frigde.getsockname()[0]) # se quiser pegar o ip, seta com o getsockname()[0]
-except Exception as e:
-    print(f"Erro ao conectar a geladeira ao servidor: {e}\n")
+            else:
+                print("Comando inválido.\n")
 
-# threads para receber os dados do servidor via TCP e para o menu da geladeira
-receive_thread = threading.Thread(target=receiveTCP, args=[]).start()
-menu_thread = threading.Thread(target=menuFrigde, args=[]).start()
-
-threads.append(receive_thread)
-threads.append(menu_thread)
+        except Exception as e:
+            print(f"Erro ao receber mensagem via TCP: {e}\n")
+            break
 
 
-
-
+# chamando a função principal
+main()
