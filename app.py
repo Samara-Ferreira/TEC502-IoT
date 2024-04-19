253bfa111e4d0cae6027ae28d714607a60352bee
Samara <sferreira@ecomp.uefs.br>
2024-04-18 21:06:33 -0300

    :sparkles: feat: adição de novas funcionalidades.

--- app.py
+++ app.py
@@ -18,91 +18,126 @@ dict_devices = {}
 
 
 # menu da aplicação
-def menu():
+def menu_application():
     os.system('cls')
     while True:
+        get_devices()
+
         print("----MENU DA APLICAÇÃO----")
+        print("[1] Selecionar um dispositivo")
+        print("[0] Fechar a aplicação")
+        #print("[-1] Desligar o broker")
+
+        option = str(input("Escolha uma opção: "))
+
+        if option == "1":
+            get_devices()
+            device_id = str(input("Digite o ID do dispositivo que deseja selecionar: "))
+            menu_device(device_id)
+        elif option == "0":
+            print("Encerrando aplicação...")
+            break
+        elif option == "-1":
+            close_api()
+
+
+# menu do dispositivo
+def menu_device(device_id):
+    os.system('cls')
+    while True:
+        get_devices()
+
+        print("----MENU DO DISPOSITIVO----")
         print("[1] Ligar dispositivo")
         print("[2] Desligar dispositivo")
-        print("[3] Mudar dados do dispositivo")
+        print("[3] Mudar dado do dispositivo")
         print("[4] Solicitar um dado do dispositivo")
         print("[5] Visualizar os dados do dispositivo")
-        print("[0] Sair")
+        print("[0] Voltar para o menu da aplicação")
         print("------------")
-        opcao = input("Escolha uma opção: ")
+        option = str(input("Escolha uma opção: "))
 
-        if opcao == "1":
-            device_id = input("Digite o ID do dispositivo que deseja ligar: ")
-            ligarDispositivo(device_id)
-        elif opcao == "2":
-            device_id = input("Digite o ID do dispositivo que deseja desligar: ")
-            desligarDispositivo(device_id)
-        elif opcao == "3":
-            device_id = input("Digite o ID do dispositivo que deseja mudar os dados: ")
-            new_data = input("Digite o novo dado: ")
-            mudarDadosDispositivo(device_id, new_data)
-        elif opcao == "4":
-            device_id = input("Digite o ID do dispositivo que deseja solicitar os dados: ")
-            solicitarDadosDispositivo(device_id)
-        elif opcao == "5":
-            device_id = input("Digite o ID do dispositivo que deseja visualizar os dados: ")
-            dados = visualizarDadosDispositivo(device_id)
-            print("DADOS ", dados)
-        elif opcao == "0":
+        if option == "1":
+            confirm = turn_on_device(device_id)
+            print(confirm)
+        elif option == "2":
+            confirm = turn_off_device(device_id)
+            print(confirm)
+        elif option == "3":
+            new_data = float(input("Digite o novo dado: "))
+            confirm = change_data_device(device_id, new_data)
+            print(confirm)
+        elif option == "4":
+            confirm = receive_data_device(device_id)
+            print(confirm)
+        elif option == "5":
+            dados = view_data_device(device_id)
+            print(dados)
+        elif option == "0":
+            print("Voltando...")
             break
         else:
             print("Opção inválida!\n")
 
+
 # função para obter os dispositivos conectados
-def obterDispositivos():
+def get_devices():
     url = f"http://{IP_API}:{PORT_API}/devices"
     response = requests.get(url)
-    print(response)
-    return response.json()
+    dict_devices = response.json()
+    print("DISPOSITIVOS CONECTADOS")
+    for key, value in dict_devices.items():
+        print(f"Dispositivo {key}: {value}")
+
 
 # função para ligar um dispositivo
-def ligarDispositivo(device_id):
-    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/ligar"
+def turn_on_device(device_id):
+    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/on"
     response = requests.post(url)
-    print(response)
-    # por que não retorna nada? print(response.json())
     return response.json()
+
 
 # função para desligar um dispositivo
-def desligarDispositivo(device_id):
-    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/desligar"
+def turn_off_device(device_id):
+    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/off"
     response = requests.post(url)
-    print(response)
     return response.json()
+
 
 # função para mudar os dados de um dispositivo
-def mudarDadosDispositivo(device_id, new_data):
-    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/mudar/{new_data}"
+def change_data_device(device_id, new_data):
+    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/change/{new_data}"
     response = requests.post(url)
-    print(response)
-    return response.json()
+    if response.content:
+        return response.json()
+    else:
+        print(response)
+        return None
+
 
 # função para solicitar os dados de um dispositivo
-def solicitarDadosDispositivo(device_id):
-    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/pegar"
+def receive_data_device(device_id):
+    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/return"
     response = requests.get(url)
-    print(response)
     return response.json()
+
 
 # função para visualizar os dados de um dispositivo
-def visualizarDadosDispositivo(device_id):
-    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/visualizar"
+def view_data_device(device_id):
+    url = f"http://{IP_API}:{PORT_API}/devices/{device_id}/view"
     response = requests.get(url)
-    print(response)
     return response.json()
 
 
+# função para fechar a aplicação
+def close_api():
+    url = f"http://{IP_API}:{PORT_API}/shutdown"
+    response = requests.post(url)
+    if response.status_code == 200:
+        print("Broker encerrado com sucesso!")
+    else:
+        print("Erro ao encerrar o Broker.")
 
-# função principal
-menu()
 
-
-
-
-
-
+if __name__ == "__main__":
+    menu_application()
