7fb8ffdb86447df94a2e04df3bc2139f7ec2ac88
Samara <sferreira@ecomp.uefs.br>
2024-04-26 20:10:52 -0300

    :recycle: refactor: correções no atuador

--- devices/actuator.py
+++ devices/actuator.py
@@ -25,61 +25,78 @@ HOST = "localhost"
 TCP_PORT = 5001
 UDP_PORT = 5002
 
-# sockets para comunicação com o servidor
-udp_frigde = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# lista de sockets para comunicação com o broker
-socketList = []
-
+socketList = ["", ""]
 
 # dicionário para armazenar os dados da geladeira
-frigde = {"ip": 0,
+fridge = {"ip": 0,
           "id": "GEL01",
           "data": 0.0,
           "category": "atuador",
           "items": {},
-          "status": False}
+          "status": False,
+          "connected": False,
+          "udp_thread": False,
+          "random_thread": False}
 
 
 # função para se conectar ao broker
 def connect_broker():
-    try:
-        tcp_frigde = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socketList.append(tcp_frigde)
+    # verificar se o dispositivo já está conectado
+    if fridge["connected"]:
+        print("ERRO: dispositivo já conectado ao broker!\n")
+    # se não estiver conectado, tentar se conectar
+    else:
+        try:
+            tcp_fridge = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            udp_fridge = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        tcp_frigde.connect((HOST, TCP_PORT))
-        threading.Thread(target=receive_tcp, args=[]).start()
-        print("Dispositivo conectado ao broker!\n")
+            tcp_fridge.connect((HOST, TCP_PORT))
 
-    except Exception as e:
-        print(f"ERRO: não foi possível se conectar com o servidor: {e}\n")
-        close_program()
+            socketList[0] = tcp_fridge
+            socketList[1] = udp_fridge
+
+            threading.Thread(target=receive_tcp, args=[]).start()
+            fridge["connected"] = True
+
+            print("Dispositivo conectado ao broker!\n")
+
+        except Exception as e:
+            print(f"ERRO: não foi possível se conectar com o servidor: {e}\n")
 
 
 # função para desconectar do broker
 def disconnect_broker():
-    try:
-        send_tcp("CLOSE")
-        time.sleep(0.5)
-        socketList[0].close()
-        socketList.clear()
-        print("Dispositivo desconectado do broker!\n")
-    except Exception as e:
-        print(f"ERRO: não foi possível desconectar do servidor: {e}\n")
+    # verificar se o dispositivo está desconectado
+    if not fridge["connected"]:
+        print("ERRO: dispositivo já desconectado do broker!\n")
+    # se não estiver desconectado, tentar desconectar
+    else:
+        try:
+            send_tcp("CLOSE")
+
+            time.sleep(1)
+
+            turn_off_fridge()
+            fridge["connected"] = False
+            socketList[0].close()
+
+            print("Dispositivo desconectado do broker!\n")
+
+        except Exception as e:
+            print(f"ERRO: não foi possível desconectar do servidor: {e}\n")
 
 
-# função para encerrar o programa
+# função para fechar o terminal do dispositivo
 def close_program():
-    try:
-        if socketList:
-            disconnect_broker()
-        udp_frigde.close()
-    except Exception as e:
-        print(f"ERRO: não fo5i possível encerrar o programa: {e}\n")
+    if fridge["connected"]:
+        disconnect_broker()
+
+    socketList[1].close()
+    print("Programa encerrado!\n")
 
 
 # menu para a geladeira no terminal do dispositivo
-def menu_frigde():
+def menu_fridge():
     while True:
 
         view_data()
@@ -102,24 +119,39 @@ def menu_frigde():
 
         # opção para ligar a geladeira
         if option == "1":
-            turn_on_frigde()
+            print("ligando a geladeira...")
+            turn_on_fridge()
 
         # opção para desligar a geladeira
         elif option == "2":
-            turn_off_frigde()
+            turn_off_fridge()
 
         # opção para mudar a temperatura da geladeira
         elif option == "3":
-            if frigde["status"] is False:
+            if fridge["status"] is False:
                 print("ERRO: não é possível modificar a temperatura, pois a geladeira está desligada!\n")
             else:
                 print("Digite a nova temperatura ")
                 data = float(input("> "))
                 change_temperature(data)
 
-        # opção para gerar valores randomicos para a temperatura (-18 a 4)
+                if not fridge["udp_thread"]:
+                    fridge["udp_thread"] = True
+                    threading.Thread(target=send_udp, args=[]).start()
+
+            # pausar a thread de valores randomicos se estiver ativa
+            if fridge["random_thread"]:
+                fridge["random_thread"] = False
+
+        # opção para gerar valores randomicos para a temperatura (-18 a 4) de forma contínua
         elif option == "4":
-            change_temperature(round(random.uniform(-18, 4), 2))
+            if fridge["status"] is False:
+                print("ERRO: não é possível modificar a temperatura, pois a geladeira está desligada!\n")
+            else:
+
+                if not fridge["random_thread"]:
+                    fridge["random_thread"] = True
+                    threading.Thread(target=random_temperature, args=[]).start()
 
         # opção para conectar ao broker
         elif option == "5":
@@ -133,7 +165,7 @@ def menu_frigde():
         elif option == "7":
             # colocar aqui validações....
             print("Digite o novo nome do dispositivo: ")
-            frigde["id"] = str(input("> "))
+            fridge["id"] = str(input("> "))
 
         # opção para encerrar o programa
         elif option == "0":
@@ -144,52 +176,76 @@ def menu_frigde():
             print("ERRO: opção inválida!\n")
 
 
+# função para gerar valores randomicos para a temperatura (-18 a 4) de forma contínua
+def random_temperature():
+    if fridge["status"] is False:
+        print("ERRO: não é possível modificar a temperatura, pois a geladeira está desligada!\n")
+
+    else:
+        if not fridge["udp_thread"]:
+            fridge["udp_thread"] = True
+            threading.Thread(target=send_udp, args=[]).start()
+
+        while fridge["status"] is True and fridge["random_thread"] is True:
+            change_temperature(round(random.uniform(-18, 4), 2))
+            time.sleep(5)
+
+
 # função para setar o endereço da geladeira
-def set_frigde_address(ip):
-    frigde["ip"] = ip
+def set_fridge_address(ip):
+    fridge["ip"] = ip
 
 
 # função para ligar a geladeira
-def turn_on_frigde():
-    frigde["status"] = True
+def turn_on_fridge():
+    fridge["status"] = True
 
 
 # função para desligar a geladeira
-def turn_off_frigde():
-    frigde["status"] = False
+def turn_off_fridge():
+    fridge["status"] = False
+    fridge["udp_thread"] = False
+    fridge["random_thread"] = False
 
 
 # função para retornar a temperatura atual
 def get_temperature():
-    return frigde["data"]
+    return fridge["data"]
 
 
 # função para retornar a quantidade de itens na geladeira
 def get_quantity():
-    return len(frigde["items"])
+    return len(fridge["items"])
 
 
 # função para mudar a temperatura da geladeira
 def change_temperature(temperature):
-    frigde["data"] = temperature
+    fridge["data"] = temperature
 
 
 # função para adicionar itens a geladeira
 def add_item(item, quantity):
-    if item in frigde["items"]:
-        frigde["items"][item] += quantity
+    if item in fridge["items"]:
+        fridge["items"][item] += quantity
     else:
-        frigde["items"][item] = quantity
+        fridge["items"][item] = quantity
 
 
 # função para remover itens da geladeira
 def remove_item(item, quantity):
+    if item not in fridge["items"]:
+        return "ERRO: ITEM NÃO ENCONTRADO\n"
     # se a quantidade for maior que a quantidade de itens, remove o item
-    if frigde["items"][item] > quantity:
-        frigde["items"][item] -= quantity
+    elif fridge["items"][item] > quantity:
+        fridge["items"][item] -= quantity
+        return "CONFIRMAÇÃO: ITEM REMOVIDO\n"
     # se a quantidade for igual a quantidade de itens, remove o item
-    elif frigde["items"][item] == quantity:
-        del frigde["items"][item]
+    elif fridge["items"][item] == quantity:
+        del fridge["items"][item]
+        return "CONFIRMAÇÃO: ITEM REMOVIDO\n"
+    # se a quantidade for menor que a quantidade de itens, retorna erro
+    else:
+        return "ERRO: QUANTIDADE DE ITENS INSUFICIENTE\n"
 
 
 # função para visualizar os dados da geladeira
@@ -197,13 +253,16 @@ def view_data():
     print("\n+" + "-" * 30 + "+")
     print("| " + " DADOS DA GELADEIRA".center(30) + "|")
     print("+" + "-" * 30 + "+")
-    print("| " + f"IP: {frigde['ip']}".ljust(30) + "|")
-    print("| " + f"ID: {frigde['id']}".ljust(30) + "|")
-    print("| " + f"Temperatura: {frigde['data']}ºC".ljust(30) + "|")
-    print("| " + f"Status: {frigde['status']}".ljust(30) + "|")
-    print("| " + f"Endereço: {frigde['ip']}".ljust(30) + "|")
-    print("| " + f"Categoria: {frigde['category']}".ljust(30) + "|")
+    print("| " + f"IP: {fridge['ip']}".ljust(30) + "|")
+    print("| " + f"ID: {fridge['id']}".ljust(30) + "|")
+    print("| " + f"Temperatura: {fridge['data']}ºC".ljust(30) + "|")
+    print("| " + f"Status: {fridge['status']}".ljust(30) + "|")
+    print("| " + f"Endereço: {fridge['ip']}".ljust(30) + "|")
+    print("| " + f"Categoria: {fridge['category']}".ljust(30) + "|")
     print("| " + f"Quantidade de itens: {get_quantity()}".ljust(30) + "|")
+    print("| " + f"Conectado: {fridge['connected']}".ljust(30) + "|")
+    print("| " + f"Thread UDP: {fridge['udp_thread']}".ljust(30) + "|")
+    print("| " + f"Thread Random: {fridge['random_thread']}".ljust(30) + "|")
     print("+" + "-" * 30 + "+\n")
 
 
@@ -212,7 +271,7 @@ def view_items():
     print("\n+" + "-" * 30 + "+")
     print("|" + " ITENS NA GELADEIRA".center(30) + "|")
     print("+" + "-" * 30 + "+")
-    for item, quantity in frigde["items"].items():
+    for item, quantity in fridge["items"].items():
         print("|" + f"{item}: {quantity}".ljust(30) + "|")
     print("+" + "-" * 30 + "+\n")
 
@@ -226,19 +285,16 @@ def send_tcp(message):
         time.sleep(2)
 
 
-# função para enviar uma mensagem via UDP
-def send_udp(message):
-    while True:
-        if frigde["status"] is False:
+def send_udp():
+    while fridge["status"] is True and fridge["udp_thread"] is True:
+        try:
+            message = f"{fridge['data']} ºC"
+            socketList[1].sendto(message.encode("utf-8"), (HOST, UDP_PORT))
+            time.sleep(5)
+        except Exception as e:
+            print(f"ERRO: não foi possível enviar a mensagem via UDP: {e}\n")
+            time.sleep(2)
             break
-        else:
-            try:
-                udp_frigde.sendto(message.encode("utf-8"), (HOST, UDP_PORT))
-                time.sleep(5)
-            except Exception as e:
-                print(f"ERRO: não foi possível enviar a mensagem via UDP: {e}\n")
-                time.sleep(2)
-                break
 
 
 # função para receber uma mensagem via TCP
@@ -250,62 +306,77 @@ def receive_tcp():
 
             # opção para setar o endereço da geladeira
             if command == "-1":
-                set_frigde_address(data)
+                set_fridge_address(str(data))
 
+            # opção para pegar os dados da geladeira
             if command == "1":
-                turn_on_frigde()
+                send_tcp(str(fridge))
+
+            # opção para ligar a geladeira
+            elif command == "2":
+                turn_on_fridge()
                 send_tcp("CONFIRMAÇÃO: GELADEIRA LIGADA\n")
 
-            elif command == "2":
-                turn_off_frigde()
+            # opção para desligar a geladeira
+            elif command == "3":
+                turn_off_fridge()
                 send_tcp("CONFIRMAÇÃO: GELADEIRA DESLIGADA\n")
 
-            elif command == "3":
-                if frigde["status"] is False:
+            # opção para mudar a temperatura da geladeira
+            elif command == "4":
+                if fridge["status"] is False:
                     send_tcp("A geladeira está desligada, não é possível mudar os dados!\n")
                 else:
                     change_temperature(float(data))
+
+                    if not fridge["udp_thread"]:
+                        fridge["udp_thread"] = True
+                        threading.Thread(target=send_udp, args=[]).start()
+
                     send_tcp("CONFIRMAÇÃO: TEMPERATURA ALTERADA\n")
 
-            elif command == "4":
-                threading.Thread(target=send_udp, args=[data]).start()
-                #send_tcp(f"CONFIRMAÇÃO: TEMPERATURA ATUAL: {get_temperature()}\n")
-
+            # opção para retornar a temperatura da geladeira
             elif command == "5":
-                send_tcp(str(frigde))
+                if not fridge["udp_thread"]:
+                    fridge["udp_thread"] = True
+                    threading.Thread(target=send_udp, args=[]).start()
 
+                send_tcp(f"CONFIRMAÇÃO: TEMPERATURA ATUAL: {get_temperature()}\n")
+
+            # opção para adicionar itens a geladeira
             elif command == "6":
                 item = data.split(",")[0]
                 quantity = int(data.split(",")[1])
+
+                print("ITEM ", item, "QUANTIDADE ", quantity)
                 add_item(item, quantity)
                 send_tcp("CONFIRMAÇÃO: ITEM ADICIONADO\n")
 
+            # opção para remover itens da geladeira
             elif command == "7":
-                if data in frigde["items"]:
-                    item, quantity = data.split(",")
-                    quantity = int(quantity)
+                item = data.split(",")[0]
+                quantity = int(data.split(",")[1])
 
-                    if frigde["items"][item] >= quantity:
-                        remove_item(item, quantity)
-                        send_tcp("CONFIRMAÇÃO: ITEM REMOVIDO\n")
-                    else:
-                        send_tcp("ERRO: QUANTIDADE DE ITENS INSUFICIENTE\n")
+                confirm = remove_item(item, quantity)
+                send_tcp(confirm)
 
+            # opção para visualizar a quantidade de itens na geladeira
             elif command == "8":
-                send_tcp(f"QUANTIDADE DE ITENS NA GELADEIRA: {get_quantity()}\n")
+                send_tcp(f"QUANTIDADE DE ITENS NA GELADEIRA: {5()}\n")
 
+            # opção para visualizar os itens da geladeira
             elif command == "9":
-                send_tcp(f"ITENS NA GELADEIRA: {frigde['items']}\n")
+                send_tcp(f"ITENS NA GELADEIRA: {fridge['items']}\n")
 
-            elif command == "0":
-                close_program()
+        # REVER
+        except Exception as e:
+            if str(e) == "[WinError 10053] Uma conexão estabelecida foi anulada pelo software no computador host":
                 break
 
-        except Exception as e:
             print(f"ERRO: não foi possível receber os dados via TCP: {e}\n")
             time.sleep(2)
             break
 
 
 # chamando a função principal
-menu_frigde()
+menu_fridge()
