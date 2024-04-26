494ed942697490cb2aa64b0f252b6036618e4c7b
Samara <sferreira@ecomp.uefs.br>
2024-04-26 20:10:18 -0300

    :recycle: refactor: correções na api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,9 @@
-""" Módulo responsável por criar a API RESTful que irá se comunicar com o broker e com o usuário """
-
 # importação de bibliotecas necessárias
 from flask import Flask, jsonify
 import server.broker as broker
+
+""" Módulo responsável por criar a API RESTful que irá se comunicar com o broker e com o usuário """
+""" REAPROVEITAR AS ROTAS DO SERVIDOR BROKER """
 
 HOST = "localhost"
 PORT = 5000
@@ -11,54 +12,54 @@ PORT = 5000
 app = Flask(__name__)
 
 
+# rota para enviar o comando de visualizar os dados de um dispositivo
+@app.route("/devices/<string:device_id>/view", methods=["GET"])
+def view_device(device_id):
+    return jsonify(broker.receive_command_api(device_id, "1", 0))
+
+
 # rota para enviar o comando de ligar um dispositivo
 @app.route("/devices/<string:device_id>/on", methods=["POST"])
 def turn_on(device_id):
-    return jsonify(broker.receive_command_api(device_id, "1", 0))
+    return jsonify(broker.receive_command_api(device_id, "2", 0))
 
 
 # rota para enviar o comando de desligar um dispositivo
 @app.route("/devices/<string:device_id>/off", methods=["POST"])
 def turn_off(device_id):
-    return jsonify(broker.receive_command_api(device_id, "2", 0))
+    return jsonify(broker.receive_command_api(device_id, "3", 0))
 
 
-# rota para enviar o comando de mudar a temperatura de um dispositivo
+# rota para enviar o comando de mudar os dados de um dispositivo
 @app.route("/devices/<string:device_id>/change/<float:new_data>", methods=["POST"])
 def change_data(device_id, new_data):
-    return jsonify(broker.receive_command_api(device_id, "3", new_data))
+    return jsonify(broker.receive_command_api(device_id, "4", new_data))
 
 
-# rota para pegar o dado de um dispositivo específico
+# rota para retornar o dado de um dispositivo
 @app.route("/devices/<string:device_id>/return", methods=["GET"])
 def return_data(device_id):
-    return jsonify(broker.receive_command_api(device_id, "4", 0))
-
-
-# rota para enviar o comando de visualizar os dados de um dispositivo específico
-@app.route("/devices/<string:device_id>/view", methods=["GET"])
-def view_device(device_id):
     return jsonify(broker.receive_command_api(device_id, "5", 0))
 
 
-# rota para obter todos os dispositivos
+# rota para obter todos os dispositivos conectados
 @app.route("/devices", methods=["GET"])
 def get_devices():
-    return jsonify(broker.receive_command_api("0", "0", 0))
+    return jsonify(broker.receive_command_api("0", "-3", 0))
 
 
 # ------------------ rotas específicas para a geladeira ------------------ #
 
 # rota para adicionar itens na geladeira
-@app.route("/devices/<string:device_id>/add/<string:item>/<int:quantity>", methods=["POST"])
-def add_item(device_id, item, quantity):
-    return jsonify(broker.receive_command_api(device_id, "6", [item, quantity]))
+@app.route("/devices/<string:device_id>/add/<string:data>", methods=["POST"])
+def add_item(device_id, data):
+    return jsonify(broker.receive_command_api(device_id, "6", data))
 
 
 # rota para remover itens da geladeira
-@app.route("/devices/<string:device_id>/remove/<string:item>", methods=["POST"])
-def remove_item(device_id, item):
-    return jsonify(broker.receive_command_api(device_id, "7", item))
+@app.route("/devices/<string:device_id>/remove/<string:data>", methods=["POST"])
+def remove_item(device_id, data):
+    return jsonify(broker.receive_command_api(device_id, "7", data))
 
 
 # rota para visualizar a quantidade de itens na geladeira
@@ -71,13 +72,6 @@ def view_quantity(device_id):
 @app.route("/devices/<string:device_id>/view/items", methods=["GET"])
 def view_items(device_id):
     return jsonify(broker.receive_command_api(device_id, "9", 0))
-
-
-# rota para encerramento da api
-@app.route("/shutdown", methods=["POST"])
-def shutdown():
-    broker.close_broker()
-    return "Servidor desligando..."
 
 
 broker.main()
