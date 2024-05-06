0e8526c5c41aa96139290ec4afbaedcca991d7f1
Samara <sferreira@ecomp.uefs.br>
2024-05-06 00:25:25 -0300

    :recycle: fix: mudança na apresentação dos dispositivos conectados

--- application/device.py
+++ application/device.py
@@ -1,3 +1,5 @@
+""" Módulo responsável por conter as funções relacionadas ao dispositivo """
+
 from time import sleep
 from api import view_data_device, all_devices
 
@@ -20,18 +22,19 @@ def view_fridge_device(device_id, ip_api, port_api):
     sleep(2)
 
 
-def get_all_devices(ip_api, port_api):
+def get_all_devices(ip_api, port_api, print_devices):
     response = all_devices(ip_api, port_api)
     if response == "None":
         print("\n\t> Nenhum dispositivo conectado!\n")
         return None
     else:
         dict_devices = eval(response)
-        print("\n\t+-" + "-" * len_print1 + "-+")
-        print("\t|" + " DISPOSITIVOS CONECTADOS".center(len_print1) + "|")
-        for device in dict_devices:
-            print("\t+-" + "-" * len_print1 + "-+")
-            print(f"\t|ID: {device}".ljust(len_print1) + "|")
-            print(f"\t|Categoria: {dict_devices[device]['category']}".ljust(len_print1) + "|")
-            print(f"\t|Conexão: {dict_devices[device]['connection']}".ljust(len_print1) + "|")
+        if print_devices:
+            print("\n\t+-" + "-" * len_print1 + "-+")
+            print("\t|" + " DISPOSITIVOS CONECTADOS".center(len_print1) + "|")
+            for device in dict_devices:
+                print("\t+-" + "-" * len_print1 + "-+")
+                print(f"\t|ID: {device}".ljust(len_print1) + "|")
+                print(f"\t|Categoria: {dict_devices[device]['category']}".ljust(len_print1) + "|")
+                print(f"\t|Conexão: {dict_devices[device]['connection']}".ljust(len_print1) + "|")
         return response
