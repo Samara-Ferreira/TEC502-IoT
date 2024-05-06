6a8f3ff86535dfdadb499f2c4e9e2871821b662d
Samara <sferreira@ecomp.uefs.br>
2024-05-06 00:20:58 -0300

    :recycle: fix: mudança nas rotas da API

--- application/api.py
+++ application/api.py
@@ -1,4 +1,4 @@
-# módulo conterá todas as funções relacionadas à API
+""" Módulo responsável por conter as funções que se comunicarão com a API RESTful """
 
 
 # importações
@@ -14,47 +14,47 @@ def all_devices(ip_api, port_api):
 
 # função para visualizar os dados da geladeira
 def view_data_device(device_id, ip_api, port_api):
-    response = requests.get(f"http://{ip_api}:{port_api}/devices/{device_id}/view")
+    response = requests.get(f"http://{ip_api}:{port_api}/{device_id}/view")
     return response.json()
 
 
 # função para ligar a geladeira
 def turn_on_device(device_id, ip_api, port_api):
-    response = requests.post(f"http://{ip_api}:{port_api}/devices/{device_id}/on")
+    response = requests.post(f"http://{ip_api}:{port_api}/{device_id}/on")
     return response.json()
 
 
 # função para desligar a geladeira
 def turn_off_device(device_id, ip_api, port_api):
-    response = requests.post(f"http://{ip_api}:{port_api}/devices/{device_id}/off")
+    response = requests.post(f"http://{ip_api}:{port_api}/{device_id}/off")
     return response.json()
 
 
 # função para mudar os dados da geladeira
 def change_data_device(device_id, new_data, ip_api, port_api):
-    response = requests.post(f"http://{ip_api}:{port_api}/devices/{device_id}/change/{new_data}")
+    response = requests.post(f"http://{ip_api}:{port_api}/{device_id}/change/{new_data}")
     return response.json()
 
 
 # função para retornar os dados da geladeira
 def receive_data_device(device_id, ip_api, port_api):
-    response = requests.get(f"http://{ip_api}:{port_api}/devices/{device_id}/return")
+    response = requests.get(f"http://{ip_api}:{port_api}/{device_id}/return")
     return response.json()
 
 
 # função para adicionar itens na geladeira
 def add_item_device(device_id, data, ip_api, port_api):
-    response = requests.post(f"http://{ip_api}:{port_api}/devices/{device_id}/add/{data}")
+    response = requests.post(f"http://{ip_api}:{port_api}/{device_id}/add/{data}")
     return response.json()
 
 
 # função para remover itens da geladeira
 def remove_item_device(device_id, data, ip_api, port_api):
-    response = requests.post(f"http://{ip_api}:{port_api}/devices/{device_id}/remove/{data}")
+    response = requests.post(f"http://{ip_api}:{port_api}/{device_id}/remove/{data}")
     return response.json()
 
 
 # função para visualizar os itens da geladeira
 def view_items_device(device_id, ip_api, port_api):
-    response = requests.get(f"http://{ip_api}:{port_api}/devices/{device_id}/view_items")
+    response = requests.get(f"http://{ip_api}:{port_api}/{device_id}/view_items")
     return response.json()
