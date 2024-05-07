cb9f0a83b135f73dbc226c4960e20741cf3cfec3
Samara <sferreira@ecomp.uefs.br>
2024-05-07 01:59:03 -0300

    :recycle: refactor: mudanças na main da aplicação

--- application/main_app.py
+++ application/main_app.py
@@ -1,17 +1,23 @@
-# este módulo importará as funções dos módulos e executará a aplicação
+""" Este arquivo é o ponto de entrada da aplicação, no qual é importado as funções dos módulos e executado a
+aplicação. """
 
+# importações das funções dos módulos
 from menu import menu_application
+from clear import clear
 
 length_print = 50
 
+# execução da aplicação
 if __name__ == "__main__":
+    clear()
+
     print("\n\t+" + "-" * length_print + "+")
     print("\t|" + " INICIANDO A APLICAÇÃO".center(length_print) + "|")
     print("\t+" + "-" * length_print + "+")
 
     print("\t>> Digite o IP do servidor: ")
-    ip_api = str(input("\t> "))
+    ip_api = str(input("\t> ")).strip()
 
-    port_api = "5555"
+    port_api = 5575
 
     menu_application(ip_api, port_api)
