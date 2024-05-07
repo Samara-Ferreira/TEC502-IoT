a377c97265ebbc674c2fcefae064f231cd47d008
Samara <sferreira@ecomp.uefs.br>
2024-05-07 01:55:01 -0300

    :recycle: refactor: refatoração das funções do dispositivo

--- application/device.py
+++ application/device.py
@@ -1,5 +1,7 @@
-""" Módulo responsável por conter as funções relacionadas ao dispositivo """
+""" Este aquivo é responsável por conter as funções relacionadas ao dispositivo, como a visualização dos dados da
+geladeira e a obtenção de todos os dispositivos conectados. """
 
+# importação de bibliotecas
 from time import sleep
 from api import view_data_device, all_devices
 
@@ -9,32 +11,33 @@ len_print1 = 50
 
 # função para visualizar os dados da geladeira
 def view_fridge_device(device_id, ip_api, port_api):
-    data = eval(view_data_device(device_id, ip_api, port_api))
-    print("\t+-" + "-" * len_print1 + "-+")
-    print("\t|" + " DADOS DA GELADEIRA".center(len_print1) + "|")
-    print("\t+-" + "-" * len_print1 + "-+")
-    print("\t|" + f"ID: {device_id}".ljust(len_print1) + "|")
-    print("\t|" + f"Status: {data['status']}".ljust(len_print1) + "|")
-    print("\t|" + f"Temperatura: {data['temperature']}".ljust(len_print1) + "|")
-    print("\t|" + f"Itens: {data['items']}".ljust(len_print1) + "|")
-    print("\t|" + f"Conexão: {data['connection']}".ljust(len_print1) + "|")
-    print("\t+-" + "-" * len_print1 + "-+\n")
-    sleep(2)
+    # obtendo os dados do dispositivo
+    data = view_data_device(device_id, ip_api, port_api)
+    # verificando se há dados
+    if data:
+        data = eval(view_data_device(device_id, ip_api, port_api))
+        print("\t+-" + "-" * len_print1 + "-+")
+        print("\t|" + " DADOS DA GELADEIRA".center(len_print1) + "|")
+        print("\t+-" + "-" * len_print1 + "-+")
+        print("\t|" + f"ID: {device_id}".ljust(len_print1) + "|")
+        print("\t|" + f"Status: {data['status']}".ljust(len_print1) + "|")
+        print("\t|" + f"Temperatura: {data['temperature']}".ljust(len_print1) + "|")
+        print("\t|" + f"Itens: {data['items']}".ljust(len_print1) + "|")
+        print("\t|" + f"Conexão: {data['connection']}".ljust(len_print1) + "|")
+        print("\t+-" + "-" * len_print1 + "-+\n")
+        sleep(2)
+    # caso não haja dados
+    else:
+        print("\n\tNão tem dispositivo conectado!\n")
 
 
-def get_all_devices(ip_api, port_api, print_devices):
+# função para obter todos os dispositivos conectados
+def get_all_devices(ip_api, port_api):
     response = all_devices(ip_api, port_api)
+    # verificando se há dispositivos conectados
     if response == "None":
         print("\n\t> Nenhum dispositivo conectado!\n")
         return None
+    # caso haja dispositivos conectados
     else:
-        dict_devices = eval(response)
-        if print_devices:
-            print("\n\t+-" + "-" * len_print1 + "-+")
-            print("\t|" + " DISPOSITIVOS CONECTADOS".center(len_print1) + "|")
-            for device in dict_devices:
-                print("\t+-" + "-" * len_print1 + "-+")
-                print(f"\t|ID: {device}".ljust(len_print1) + "|")
-                print(f"\t|Categoria: {dict_devices[device]['category']}".ljust(len_print1) + "|")
-                print(f"\t|Conexão: {dict_devices[device]['connection']}".ljust(len_print1) + "|")
         return response
